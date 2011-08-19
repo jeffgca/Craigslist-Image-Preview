6b12ed51aa43a49810a82463af750aadc8b9c553
Jeff Griffiths <jeff@canuckistani.ca>
2011-08-18 22:27:09 -0700

    initial commit

---
+++ data/mod_script.js
@@ -0,0 +1,14 @@
+
+var clPreviewInit = function() {
+
+    /*
+     * 1. iterate the list
+     * 2. fetch the page for each link
+     * 4. display thumbnails of pics
+     * 5. some kind of lightbox
+     */
+    $('p.row').each(function() {
+        // $(this).children
+    })
+
+}
